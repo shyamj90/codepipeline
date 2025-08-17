325cfbce81142eb3c037351eaf94cff2e94af005
Divya Shukla <divyashukla20993@gmail.com>
2025-08-17 10:22:10 +0530

    test

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (_req, res) => res.send('Hello from CodePipeline → CodeBuild → CodeDeploy → EC2!'));
-app.get('/health', (_req, res) => res.status(200).send('OK'));
-
-app.listen(port, () => console.log(`App listening on ${port}`));
